cbc2f62e2107a4494bad7e94f06e061e52bf6c85
vivek singh <vikky30singh@gmail.com>
2021-02-21 15:52:11 +0530

    implemented search functionality

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
+search_box = None
+search_button = None
+
+query = input('Which thing would you like to search: ').strip().lower()
+platform = input(f'From which platfrom you wanted to search {query}: ').strip().lower()
+
+if not query or not platform:
+    print('please enter required information to procced')
+    exit()
+
 driver = webdriver.Chrome()
-driver.get('https://www.youtube.com')
+
+driver.get(f'https://{platform}.com')
+driver.maximize_window()
+
+if platform == 'youtube':
+    search_box = driver.find_element_by_id('search')
+elif platform in ['github','stackoverflow','google']:
+    search_box = driver.find_element_by_name('q')
+else:
+    print('the platform you are looking for is not in our list try something else')
+    exit()
+
+if search_box:
+    search_box.send_keys(query)
+    search_box.send_keys(Keys.RETURN)
